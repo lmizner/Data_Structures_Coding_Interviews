dd20db13359f1d2f9e439d4d2eced89813204d20
Lauren Mizner <74742038+lmizner@users.noreply.github.com>
2024-11-04 10:23:26 -0600

    Add files via upload

---
+++ Trie/trie_total_words.py
@@ -0,0 +1,190 @@
+class TrieNode:
+    def __init__(self, char=''):
+        self.children = [None] * 26  # This will store pointers to the children
+        self.is_end_word = False  # true if the node represents the end of word
+        self.char = char  # To store the value of a particular key
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()  # Root node
+
+    # Function to get the index of character 't'
+    def get_index(self, t):
+        return ord(t) - ord('a')
+
+    # Function to insert a key in the Trie
+    def insert(self, key):
+        if key is None:
+            return False  # None key
+
+        key = key.lower()  # Keys are stored in lowercase
+        current = self.root
+
+        # Iterate over each letter in the key
+        # If the letter exists, go down a level
+        # Else simply create a TrieNode and go down a level
+        for letter in key:
+            index = self.get_index(letter)
+
+            if current.children[index] is None:
+                current.children[index] = TrieNode(letter)
+
+            current = current.children[index]
+
+        current.is_end_word = True
+
+    # Function to search a given key in Trie
+    def search(self, key):
+        if key is None:
+            return False  # None key
+
+        key = key.lower()
+        current = self.root
+
+        # Iterate over each letter in the key
+        # If the letter doesn't exist, return False
+        # If the letter exists, go down a level
+        # We will return true only if we reach the leafNode and
+        # have traversed the Trie based on the length of the key
+
+        for letter in key:
+            index = self.get_index(letter)
+            if current.children[index] is None:
+                return False
+            current = current.children[index]
+
+        if current is not None and current.is_end_word:
+            return True
+
+        return False
+
+    # Recursive function to delete given key
+    def delete_helper(self, key, current, length, level):
+        deleted_self = False
+
+        if current is None:
+            print("Key does not exist")
+            return deleted_self
+
+        # Base Case:
+        # We have reached at the node which points
+        # to the alphabet at the end of the key
+        if level is length:
+            # If there are no nodes ahead of this node in
+            # this path, then we can delete this node
+            print("Level is length, we are at the end")
+            if current.children.count(None) == len(current.children):
+                print("- Node", current.char, ": has no children, delete it")
+                current = None
+                deleted_self = True
+
+            # If there are nodes ahead of current in this path
+            # Then we cannot delete current. We simply unmark this as leaf
+            else:
+                print("- Node", current.char, ": has children, don't delete \
+                it")
+                current.is_end_word = False
+                deleted_self = False
+
+        else:
+            index = self.get_index(key[level])
+            print("Traverse to", key[level])
+            child_node = current.children[index]
+            child_deleted = self.delete_helper(
+                key, child_node, length, level + 1)
+            # print( "Returned from", key[level] , "as",  child_deleted)
+            if child_deleted:
+                # Setting children pointer to None as child is deleted
+                print("- Delete link from", current.char, "to", key[level])
+                current.children[index] = None
+                # If current is a leaf node then
+                # current is part of another key
+                # So, we cannot delete this node and it's parent path nodes
+                if current.is_end_word:
+                    print("- - Don't delete node", current.char, ": word end")
+                    deleted_self = False
+
+                # If child_node is deleted and current has more children
+                # then current must be part of another key
+                # So, we cannot delete current Node
+                elif current.children.count(None) != len(current.children):
+                    print("- - Don't delete node", current.char, ": has \
+                    children")
+                    deleted_self = False
+
+                # Else we can delete current
+                else:
+                    print("- - Delete node", current.char, ": has no children")
+                    current = None
+                    deleted_self = True
+
+            else:
+                deleted_self = False
+
+        return deleted_self
+
+    # Function to delete given key from Trie
+    def delete(self, key):
+        if self.root is None or key is None:
+            print("None key or empty trie error")
+            return
+        print("\nDeleting:", key)
+        self.delete_helper(key, self.root, len(key), 0)
+
+
+
+######################################################################
+
+
+
+def total_words(root):
+    
+    # Initialize variable to keep count of words
+    results = 0
+
+    # A leaf denotes the end of a word, if found add to count
+    if root.is_end_word:
+        results += 1
+
+    # Recursively traverse all non-null children nodes of the current node
+    for letter in root.children:
+        if letter is not None:
+            results += total_words(letter)
+    
+    return results
+
+
+
+# Time Complexity = O(n) with n being total number of nodes
+# Space Complexity = O(m) with m being total number of words
+
+
+
+######################################################################
+
+
+
+def main():
+    num = 1
+    keys = [["hello", "world", "python", "programming"],
+            ["app", "apple", "applepie"],
+            ["dog", "dig", "ham", "hamster", "dino"],
+            ["sun", "burn"],
+            ["red", "blue", "green", "yellow", "orange", "purple"]]
+    
+    for words in keys:
+        t = Trie()
+        for word in words:
+            t.insert(word)
+            num += 1
+            words_found = total_words(t.root)
+        print("\nTotal words found: ", words_found, sep="")
+        print("-" * 50)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
